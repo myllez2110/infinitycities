6fed709d33769dfefd51dd7ae1b34edc8053d6d7
Daniel Baptista <bapniel21@gmail.com>
2024-12-19 14:13:47 -0300

    update package and config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/infinitecities/',
+  base: '/infinitycities/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
